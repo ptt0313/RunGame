b06893a597f0bbfe9212fcba9bf0d3efe22dbb6e
ptt0313 <ptt0313@gmail.com>
2024-10-01 15:46:31 +0900

    24.10.01 장애물 관리자 시스템

---
+++ Assets/Scripts/Managers/MouseManager.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MouseManager : Singleton<MouseManager>
+{
+    [SerializeField] Texture2D texture2D;
+    void Start()
+    {
+        texture2D = ResourcesManager.Instance.Load<Texture2D>("Default");
+        Cursor.SetCursor(texture2D, Vector2.zero, CursorMode.Auto);
+    }
+    private void OnEnable()
+    {
+        SceneManager.sceneLoaded += OnSceneLoaded;
+    }
+    private void OnDisable()
+    {
+        SceneManager.sceneLoaded -= OnSceneLoaded;
+
+    }
+
+    void OnSceneLoaded(Scene scene, LoadSceneMode mode)
+    {
+        State(scene.buildIndex);
+    }
+    public void State(int state)
+    {
+        switch (state)
+        {
+            case 0: Cursor.visible = true;
+                Cursor.lockState = CursorLockMode.None;
+                break;
+            case 1: Cursor.visible = false;
+                Cursor.lockState = CursorLockMode.Locked;
+                
+                break;
+        }
+    }
+}
--- Assets/Scripts/Managers/ObstacleManager.cs
+++ Assets/Scripts/Managers/ObstacleManager.cs
@@ -8,10 +8,12 @@ public class ObstacleManager : MonoBehaviour
     [SerializeField] GameObject prefab;
 
     [SerializeField] int creatCount = 5;
+    [SerializeField] int random;
     void Start()
     {
         obstacles.Capacity = 10;
         Creat();
+        StartCoroutine(ActiveObstacle());
     }
     public void Creat()
     {
@@ -20,6 +22,43 @@ public class ObstacleManager : MonoBehaviour
             prefab = ResourcesManager.Instance.Instantiate("Corn", gameObject.transform);
             prefab.SetActive(false);
             obstacles.Add (prefab);
+        }
+    }
+    public bool ExamineActive()
+    {
+        for (int i = 0; i < obstacles.Count; i++)
+        {
+            if (obstacles[i].activeSelf == false)
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+    public IEnumerator ActiveObstacle()
+    {
+        while(true)
+        {
+            yield return CoroutinCache.waitForSecond(2.5f);
+            random = Random.Range(0, obstacles.Count);
+            // 현재 게임 오브젝트가 활성화 되어 있는지 확인합니다.
+            
+            
+            while (obstacles[random].activeSelf == true)
+            {
+                // 현재 리스트에 있는 모든 게임 오브젝트가 활성화되어 있는 지 확인합니다.
+                if (ExamineActive() == true)
+                {
+                    GameObject clone = ResourcesManager.Instance.Instantiate("Corn", gameObject.transform);
+                    clone.SetActive(false);
+                    obstacles.Add(clone);
+                }
+
+                // 현재 리스트에 있는 게임 오브젝트가 활성화되어 있으면 random 변수의 값을 +1 해서 다시 검색 합니다.
+                random = (random + 1) % obstacles.Count;
+            }
+            // 랜덤으로 설정된 Obstacle 오브젝트를 활성화합니다.
+            obstacles[random].SetActive(true);
         }
     }
 }
